c4c51058515cd6a2904887f8a20ecb17c69245a4
marshallcode08 <ryanmarshall@Ryans-iMac.lan>
2022-09-28 22:53:35 -0400

    Adding PyPoll.py file.

---
+++ Pyhton_practice.py
@@ -0,0 +1,14 @@
+print("Hello World")
+# How many votes did you get?
+#counmy_votes = int(input("How many votes did you getin the election"))
+# Total votes in the election
+#total_votes = int(input("What is the total votes in the election"))
+#calculate the percentage of votes you received
+#percentages_votes = (my_votes / total_votes) * 100
+#print("I received" + str(percentage_votes)+ "% of the total votes.")
+#countries =["Arapahoe","Denver","Jefferson"]
+temperature = int(input("What is the temperature outside?"))
+if temperature > 80
+    print("Turn on the AC.")
+Else:
+     print("Open the windows")    
